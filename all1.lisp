09cd904a066a5b539161e9bcc21c91d638303d9e
jccerrillo <jccerrillo@gmail.com>
2016-12-11 21:36:07 -0600

    updated all1.lisp ql directive for clm-directory

--- all1.lisp
+++ all1.lisp
@@ -33,20 +33,20 @@
 
 (proclaim '(special clm-directory clm-bin-directory))
 
-#|
+#-quicklisp
 (if (not (boundp 'clm-directory)) 
     (setf clm-directory 
       (namestring
        (truename 
 	(directory-namestring (or *load-pathname* "./"))))))
-|#
+
 
 #+openmcl (if (and (stringp clm-directory)
 		   (> (length clm-directory) 0)
 		   (not (char= (elt clm-directory (1- (length clm-directory))) #\/)))
 	      (setf clm-directory (concatenate 'string clm-directory "/")))
 
-
+#+quicklisp
 (setf clm-directory (namestring (ql:where-is-system :clm)))
 (if (not (boundp 'clm-bin-directory)) (setf clm-bin-directory clm-directory))
 
